a33f7575dcdbb792f64f318537024fbb76759d36
Elias Dorigoni <eliasdorigoni@gmail.com>
2021-08-14 23:49:39 -0300

    Agrega grilla para ver las velocidades

--- src/world/index.js
+++ src/world/index.js
@@ -1,5 +1,7 @@
+import options from './../config/options.yaml'
+
 const World = {
-    init: () => {
+    init: function() {
         const element = document.getElementById('world')
         let context = element.getContext('2d', { alpha: false })
         context.fillStyle = "black"
@@ -10,7 +12,30 @@ const World = {
             context.drawImage(bg, 0, 0)
         }
         bg.src = require('./background.svg')
-    }
+    },
+    grid: function() {
+        const context = document.getElementById('grid').getContext("2d");
+        context.beginPath()
+
+        let x = 0,
+            y = 0
+
+        for (let i = 0; i < 29; i++) {
+            context.strokeStyle = '#333333';
+            context.moveTo(x, 0)
+            context.lineTo(x, 992)
+            context.stroke()
+            x += options.tileSize
+        }
+
+        for (let i = 0; i < 29; i++) {
+            context.strokeStyle = '#333333';
+            context.moveTo(0, y)
+            context.lineTo(896, y)
+            context.stroke()
+            y += options.tileSize
+        }
+    },
 }
 
 export default World
